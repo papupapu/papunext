0883d409bf7debf393c6398e27f429c1cc11e0bf
Giancarlo Gerbaz <ganesa_2001@yahoo.it>
2020-12-11 18:23:10 +0100

    progress

--- pages/index.js
+++ pages/index.js
@@ -5,24 +5,50 @@ import Footer from '../components/Footer';
 
 import styles from '../styles/Home.module.scss';
 
+function Item() {
+  return (
+    <div className={styles['splash__triplet--itm']}>
+      <picture className={styles.splash__pic}>
+        <source
+          media="(min-width:768px)"
+          srcSet="https://res.cloudinary.com/dia4050i1/image/upload/v1491757981/surf/1000x751/teahupoo2.jpg"
+        />
+        <img
+          src="https://res.cloudinary.com/dia4050i1/image/upload/v1491758045/surf/345X259/teahupoo2.jpg"
+          width="345"
+          height="259"
+          alt=""
+        />
+      </picture>
+    </div>
+  );
+} 
+
+function Triplet() {
+  return(
+    <div className={styles.splash__triplet}>
+      <div className={styles['splash__triplet--big']}>
+        <Item />
+      </div>
+      <div className={styles['splash__triplet--reg']}>
+        <Item />
+        <Item />
+      </div>
+    </div>
+  );
+}
+
 export default function Home() {
   return (
     <div className="glob_container">
       <Header />
       <div className="glob_content">
         <div className={styles.splash}>
-          <picture className={styles.splash__pic}>
-            <source
-              media="(min-width:768px)"
-              srcSet="https://res.cloudinary.com/dia4050i1/image/upload/v1491757981/surf/1000x751/teahupoo2.jpg"
-            />
-            <img
-              src="https://res.cloudinary.com/dia4050i1/image/upload/v1491758045/surf/345X259/teahupoo2.jpg"
-              width="345"
-              height="259"
-              alt=""
-            />
-          </picture>
+          <div className={styles.splash__slider}>
+            <Triplet />
+            <Triplet />
+            <Triplet />
+          </div>
         </div>
         <div className={styles.list}>
           <div className={styles.list__card}>ya</div>
