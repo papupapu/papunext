f367ef03f34a617e0e01b94b78c3a5ea2f0ecc8d
Giancarlo Gerbaz <giancarlo.gerbaz@casa.it>
2020-12-22 07:59:06 +0100

    add img to cards

--- components/Cards/Ticket/index.js
+++ components/Cards/Ticket/index.js
@@ -5,11 +5,13 @@ import styles from './tk.module.scss';
 function Ticket() {
   const t = `Tamara Lunger atterrata a Islamabad e pronta per il K2`;
   const p = `Un'articolo di Ciccio pasticcio`;
+  const d = `Un'articolo di Ciccio pasticcio articolo di Ciccio pasticcio articolo di Ciccio pasticcio articolo di Ciccio pasticcio articolo di Ciccio pasticcio`;
   return (
     <div className={styles.card}>
       <div className={styles.card__cnt}>
         <h1>{t}</h1>
         <p>{p}</p>
+        <div />
       </div>
       <div className={styles.card__holes}>
         <div
@@ -19,7 +21,9 @@ function Ticket() {
           className={`${styles['card__holes--hole']} ${styles['card__holes--right']}`}
         />
       </div>
-      <div className={styles.card__foot} />
+      <div className={styles.card__foot} style={{ textAlign: 'left' }}>
+        {d}
+      </div>
     </div>
   );
 }
