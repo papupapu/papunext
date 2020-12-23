b28b3f9dac40739df889db93cbb0f9f4371914de
Giancarlo Gerbaz <giancarlo.gerbaz@casa.it>
2020-12-23 15:49:24 +0100

    add new component

---
+++ components/HP/Slides/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import styles from './slides.module.scss';
+
+function Slides() {
+  return (
+    <div className={`${styles.slides} mb--m`}>
+      <div className={styles.slides__cont}>
+        <div className="c-bg--p" />
+        <div className="c-bg--p" />
+        <div className="c-bg--p" />
+        <div className="c-bg--p" />
+        <div className="c-bg--p" />
+      </div>
+    </div>
+  );
+}
+export default Slides;
