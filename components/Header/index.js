a71e224155a998f7366e9b1e960b6f1ee58ca104
Giancarlo Gerbaz <giancarlo.gerbaz@casa.it>
2020-12-28 21:42:20 +0100

    slider typography & responsive optimization

--- components/Header/index.js
+++ components/Header/index.js
@@ -4,18 +4,20 @@ import styles from './header.module.scss';
 
 export default function Header() {
   return (
-    <div className={`${styles.header} pl--m pr--m c-bg--w`}>
-      <div className={styles.header__logo}>Logo</div>
-      <div className={styles.header__nav}>
-        <a href="/" title="Primo link" className="c-txt--f2">
-          Primo link
-        </a>
-        <a href="/" title="Secondo link" className="c-txt--f2">
-          Secondo link
-        </a>
-        <a href="/" title="Terzo link" className="c-txt--f2">
-          Terzo link
-        </a>
+    <div className={`${styles.header} c-bg--p`}>
+      <div className={`${styles.header__wrap} pl--m pr--m`}>
+        <div className={styles.header__logo}>Logo</div>
+        <div className={styles.header__nav}>
+          <a href="/" title="Primo link" className="c-txt--f2">
+            Primo link
+          </a>
+          <a href="/" title="Secondo link" className="c-txt--f2">
+            Secondo link
+          </a>
+          <a href="/" title="Terzo link" className="c-txt--f2">
+            Terzo link
+          </a>
+        </div>
       </div>
     </div>
   );
